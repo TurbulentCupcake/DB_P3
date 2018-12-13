aef830e3d8b1a44a681ab77032f5dd3070c08c74
TurbulentCupcake <muraliadithya315@gmail.com>
2018-12-13 16:15:43 -0600

    time is work

--- auctionbase/create_auctionbase/runParser.sh
+++ auctionbase/create_auctionbase/runParser.sh
@@ -1,4 +1,4 @@
-python skeleton_parser.py /usr/class/cs145/project/ebay_data/items-*.json
+python2 skeleton_parser.py /home/adithya/Desktop/"FALL 18"/CS564/Projects/P3/auctionbase/ebay_data/items-*.json
 
 sort -u items.dat -o itemsUnique.dat
 sort -u categories.dat -o categoriesUnique.dat
